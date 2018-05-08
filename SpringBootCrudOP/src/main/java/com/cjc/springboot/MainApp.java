22b9824f88f0dce3bd5e1e6ea5dbaf898f1dc1fd
Swapnil <Swapnil@Swapnil-PC>
2018-05-08 13:02:55 +0530

    dsdads

--- SpringBootCrudOP/src/main/java/com/cjc/springboot/MainApp.java
+++ SpringBootCrudOP/src/main/java/com/cjc/springboot/MainApp.java
@@ -3,13 +3,13 @@ package com.cjc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com")
 public class MainApp {
 
 	public static void main(String[] args) {
+		System.out.println("in main method");
 		
-		
-		SpringApplication.run(MainApp.class, args);
+	SpringApplication.run(MainApp.class, args);
 		
 		
 	}
